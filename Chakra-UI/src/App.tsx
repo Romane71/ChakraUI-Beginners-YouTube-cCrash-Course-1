ebafe3dfa2aa2e24891f448aa5d5d3affe4d1ec4
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-24 00:44:13 +0000

    Box component

--- Chakra-UI/src/App.tsx
+++ Chakra-UI/src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Box } from "@chakra-ui/react"; // This is how you import each component from chakra
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Box as="section" bg="green" w="12opx" h="150px">
+        This is a box
+      </Box>
+    </div>
+  );
 }
 
 export default App;
