47f1feaba476fbe11fb2b3cc192ac56ec8a1221e
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-24 00:48:01 +0000

    Flex + Box

--- Chakra-UI/src/App.tsx
+++ Chakra-UI/src/App.tsx
@@ -1,13 +1,23 @@
-import { Box } from "@chakra-ui/react"; // This is how you import each component from chakra
-
 import "./App.css";
+import { Box, Flex } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <Box as="section" bg="green" w="12opx" h="150px">
-        This is a box
-      </Box>
+      <Flex justify="space-between" align="center">
+        <Box bg="green" w="100px" h="100px">
+          item1
+        </Box>
+        <Box bg="green" w="100px" h="200px">
+          item2
+        </Box>
+        <Box bg="red" w="200px" h="200px">
+          item3
+        </Box>
+        <Box bg="orange" w="200px" h="200px">
+          item4
+        </Box>
+      </Flex>
     </div>
   );
 }
