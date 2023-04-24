d651059c39ba5c79f0ad578cff87ffe072c8210e
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-24 01:07:31 +0000

    Pseudo style props

--- Chakra-UI/src/App.tsx
+++ Chakra-UI/src/App.tsx
@@ -1,23 +1,29 @@
 import "./App.css";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Text, Button } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <Flex justify="space-between" align="center">
-        <Box bg="green" w="100px" h="100px">
-          item1
-        </Box>
-        <Box bg="green" w="100px" h="200px">
-          item2
-        </Box>
-        <Box bg="red" w="200px" h="200px">
-          item3
-        </Box>
-        <Box bg="orange" w="200px" h="200px">
-          item4
-        </Box>
-      </Flex>
+      <Text as="i" fontSize="32">
+        Hi there! I am Romane and I am French{" "}
+      </Text>
+
+      <Box bg="green" w="100px" h="100px" margin="auto" mt="10px">
+        item1
+        <Button mt="32px" colorScheme="red" _hover={{ background: "blue" }}>
+          Click me!
+        </Button>
+      </Box>
+      <Box bg="green" w="100px" h="200px">
+        item2
+      </Box>
+      <Box bg="red" w="200px" h="200px">
+        item3
+      </Box>
+      <Box bg="orange" w="200px" h="200px">
+        item4
+      </Box>
     </div>
   );
 }
